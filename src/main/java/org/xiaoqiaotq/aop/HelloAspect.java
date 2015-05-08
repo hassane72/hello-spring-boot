a95b7a55095005f8d62958e09fd6dab8b5017065
xiaoqiaotq <xiaoqiaotq@gmail.com>
2015-05-08 17:56:08 +0800

    aop支持

---
+++ src/main/java/org/xiaoqiaotq/aop/HelloAspect.java
@@ -0,0 +1,27 @@
+package org.xiaoqiaotq.aop;
+
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * author: xiaoqiaotq@gmail.com
+ * date  : 2015/5/8.
+ */
+@Aspect
+@Component
+public class HelloAspect {
+    @Pointcut("execution(* org.xiaoqiaotq.service.*.*(..))")
+    public void p1(){}
+
+    @Around("p1()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.err.println("-------fffddd--------------");
+        System.err.println(joinPoint.getSignature());
+        Object proceed = joinPoint.proceed();
+        return proceed;
+    }
+}
