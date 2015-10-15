b6d19e19860c248ba7a51f966a7440dcd6752ba4
xiaoqiaotq <xiaoqiaotq@gmail.com>
2015-10-15 16:08:29 +0800

    update

---
+++ src/main/java/org/xiaoqiaotq/repository/StreetRepository.java
@@ -0,0 +1,13 @@
+package org.xiaoqiaotq.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.xiaoqiaotq.domain.geom.StreetTest;
+
+/**
+ * author: xiaoqiaotq@gmail.com
+ * date  : 2015/10/15
+ */
+@Repository
+public interface StreetRepository extends JpaRepository<StreetTest,Long> {
+}
